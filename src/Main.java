cb9516c7b1c59f78dc9e259f6c49f9afd6bf7833
arceeluceno13 <arceeluceno13@gmail.com>
2023-12-09 02:19:56 +0800

    JHEN BOBO

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import classes.HangmanGame;
 import classes.NumberGuessingGame;
 import classes.RockPaperScissors;
+
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         HangmanGame hangman = new HangmanGame();
@@ -13,14 +14,14 @@ public class Main{
         System.out.print("Pick a game from 1-3!: ");
         int game = scanner.nextInt();
 
-        switch(game){
-            case 1 :
+        switch (game) {
+            case 1:
                 hangman.play();
                 break;
-            case 2 :
+            case 2:
                 number.play();
                 break;
-            case 3 :
+            case 3:
                 rockpaperscissors.playGame();
                 break;
             default:
@@ -28,3 +29,5 @@ public class Main{
         }
     }
 }
+
+//YAWA
